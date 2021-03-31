39217c919c39691bec4151daedbfcc3f5556b693
Chad Tarpey <chadtarpey@gmail.com>
2021-03-31 17:28:07 -0500

    created requests.js and got apikey

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "81d8e294631eb763043051e297c1fb2d";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    FecthHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    FecthHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    FecthDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
