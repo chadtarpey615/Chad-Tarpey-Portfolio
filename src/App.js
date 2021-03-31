d7360d17e43a11479b302b269ede3f9efe1cf2a5
Chad Tarpey <chadtarpey@gmail.com>
2021-03-31 17:42:49 -0500

    created axios.js and Row.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import './App.css';
+import Row from "./Row"
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
     </div>
   );
 }
