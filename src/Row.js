58a008d67c6e91bda94f76b24a192cb0979227f5
Chad Tarpey <chadtarpey@gmail.com>
2021-04-01 15:31:54 -0500

    created row.css and got a endpoint on fetch

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from "./axios.js";
+import "./row.css";
 
-function Row({ title }) {
+const base_url = "https://image.tmdb.org/t/p/original/"
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
+
+    // a snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData()
+    }, [fetchUrl]);
+
+    console.log(movies)
     return (
-        <div>
+        <div className="row">
             {/* title */}
             <h2>{title}</h2>
-            {/* container -> posters */}
+            <div className="row__posters">
+                {/* several row posters */}
+                {movies.map(movie => (
+                    <img
+                        className="row__poster"
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
+
+
 
             {/*  */}
         </div>
