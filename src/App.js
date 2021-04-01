db0807200d3b38c67b543612d8f83a79e84b2f1f
Chad Tarpey <chadtarpey@gmail.com>
2021-04-01 16:26:58 -0500

    created Banner.js and banner.css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import './App.css';
 import Row from "./Row"
 import requests from "./request";
+import Banner from "./Banner";
 
 function App() {
   return (
+
     <div className="App">
+      {/* navbar */}
+      <Banner />
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
